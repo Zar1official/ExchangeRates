d2ca99a67b101ae7308355ac4df063cdda5f380b
Zar <dimzar233445@gmail.com>
2022-07-27 01:38:15 +0300

    clean main activity

--- app/src/main/java/ru/zar1official/exchangerates/presentation/screens/navigation/MainActivity.kt
+++ app/src/main/java/ru/zar1official/exchangerates/presentation/screens/navigation/MainActivity.kt
@@ -4,15 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import dagger.hilt.android.AndroidEntryPoint
-import ru.zar1official.exchangerates.domain.usecases.AddFavouriteUseCase
 import ru.zar1official.exchangerates.presentation.theme.ExchangeRatesTheme
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-    @Inject
-    lateinit var useCase: AddFavouriteUseCase
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
